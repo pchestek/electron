52031f171e29f97642a72d64601baf2c9a1a7f77
thomas.moutel <thomas.moutel@thomsonreuters.com>
2016-11-29 17:20:53 +0100

    JIRA: Story SAP-117 Unassigned Development in Progress [Container framework,Messenger,Multi-platform Eikon,Scripting proxy RRG] [IPC] Electron App Test
    * Better UI to reflect ipcBus calls & answers: added some colors & a timer to slow it down
    
    Risk: Low
    Impacts: None

--- tr-apps/ipc-bus-tests/TableView.js
+++ tr-apps/ipc-bus-tests/TableView.js
@@ -1,5 +1,10 @@
 var processes = [ "###", "master", "renderer1", "renderer2", "nodeA", "nodeB" ];
 
+initialize = function(htmlTable) {
+    initView(htmlTable);
+    initProcesses();
+};
+
 initView = function(htmlTable) {
     processes.forEach(function(nameRow) {
         let newRow = document.createElement("tr");
@@ -9,7 +14,7 @@ initView = function(htmlTable) {
             newRow.setAttributeNode(classFirstRow);
         }
         else {
-            doSubscribe(nameRow);
+            initProcess(nameRow);
         }
         processes.forEach(function(nameCol) {
             let newCol = document.createElement("td");
@@ -44,20 +49,58 @@ initView = function(htmlTable) {
     });
 };
 
+initProcesses = function() {
+    ;
+};
+
+initProcess = function(processName) {
+    if (processName.lastIndexOf("master") == 0) {
+        // console.debug("master: '%s'", processName);
+        doSubscribe(processName);
+    }
+    else if (processName.lastIndexOf("renderer") == 0) {
+        createRenderer(processName);
+    }
+    else if (processName.lastIndexOf("node") == 0) {
+        createNodeProcess(processName);
+    }
+};
+
+createNodeProcess = function(processName) {
+    // console.debug("node: '%s'", processName);
+    doSubscribe(processName);
+};
+
+createRenderer = function(processName) {
+    // console.debug("renderer: '%s'", processName);
+    doSubscribe(processName);
+};
+
 onCellClicked = function(emitter, receiver) {
-    // to be replaced with calls to ipcBus
-    console.info("Cell clicked: %s/%s", emitter, receiver);
-    ipcBus.send(receiver, emitter);
+    // console.debug("Cell clicked: %s/%s", emitter, receiver);
+    let cellId = "IPC_TEST_" + emitter + "_" + receiver;
+    let sourceCell = document.getElementById(cellId);
+    let backgroundColor = document.createAttribute("style");
+    backgroundColor.value = "color:white;background-color:red";
+    sourceCell.setAttributeNode(backgroundColor);
+    sourceCell.innerHTML = "Testing..."; 
+    // topic is the receiver, message is the emitter
+    setTimeout(function() {
+        ipcBus.send(receiver, emitter);
+    }, 500);
 };
 
 doSubscribe = function(topic) {
+    // console.debug("Subscribed to '%s'", topic);
     ipcBus.subscribe(topic, onIPCMessageReceived);
-    // console.log("Subscribed to '%s'", topic);
 };
 
 onIPCMessageReceived = function(topic, message) {
-    // console.info("Message received on '%s': '%s'", topic, message);
+    // console.debug("Message received on '%s': '%s'", topic, message);
     let cellId = "IPC_TEST_" + message + "_" + topic;
     let targetCell = document.getElementById(cellId);
+    let backgroundColor = document.createAttribute("style");
+    backgroundColor.value = "color:white;background-color:green";
+    targetCell.setAttributeNode(backgroundColor);
     targetCell.innerHTML = "Success"; 
 };
